16be47ef532b93da55d6f0feddd1b6c8852e9926
iYeremy <yeremyvegap@gmail.com>
2026-06-15 03:55:11 -0500

    feat: implement father-son proccess and stdout.flush

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use std::io::stdin; // importar i/o
 use std::io::stdout;
-use std::io::Write;
+use std::io::Write; // necesario para el flush
 use std::process::Command; // importar struct Command 
 fn main() {
     loop{
         print!("osh :3 >>"); // imprime sin salto de linea
-        stdout().flush(); 
+        stdout().flush(); // vacia el bufer de salida. forzando a que el texto se imprima inmediatamente en la consola  
         let mut input = String::from("");
         stdin().read_line(&mut input).unwrap();
 
         let command = input.trim(); // .trim elimina los espacios en blanco, tabulaciones y saltos de linea
 
-        Command::new(command).spawn().unwrap();
+        
+        let mut command_child = Command::new(command).spawn().unwrap();
+
+        command_child.wait(); // espera hasta que el proceso hijo termine de completarse para asi continuar el floop
     }
 }
